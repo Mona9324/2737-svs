a058071c18fa823aa5f6491df1a3b47687b65877
Mona9324 <qoekfthsu9@gmail.com>
2026-03-15 19:52:05 +0900

    app.js

--- app.js
+++ app.js
@@ -1,65 +1,25 @@
+let currentBuff="construction"
+let selectedSlot=null
+
 const ADMIN_PASSWORD="2737admin"
 
+const grid=document.getElementById("slots")
 
 const allianceColors={}
-
-const colorList=[
-"#9db4ff",
-"#ffb6b6",
-"#ffd6a5",
-"#caffbf",
-"#bdb2ff",
-"#ffc6ff"
-]
+const colorList=["#8fb3ff","#ffb3b3","#ffd6a5","#caffbf","#d0b3ff"]
 
 function getAllianceColor(tag){
 
 if(!allianceColors[tag]){
 
-let color=getAllianceColor(data.alliance)
-
-div.innerHTML=
-"<b>"+time+" UTC</b><br>"+
-local+" Local<br>"+
-"<span style='color:"+color+"'>["+data.alliance+"]</span> "+
-data.player
+let index=Object.keys(allianceColors).length%colorList.length
+allianceColors[tag]=colorList[index]
 
 }
 
 return allianceColors[tag]
 
 }
-
-
-let currentBuff="construction"
-
-const grid=document.getElementById("slots")
-
-const svsDate=new Date("2030-03-23T00:00:00Z")
-
-let selectedSlot=null
-
-function countdown(){
-
-let now=new Date()
-let diff=svsDate-now
-
-if(diff<0){
-document.getElementById("countdown").innerHTML="SVS Started!"
-return
-}
-
-let d=Math.floor(diff/(1000*60*60*24))
-let h=Math.floor((diff/(1000*60*60))%24)
-let m=Math.floor((diff/(1000*60))%60)
-
-document.getElementById("countdown").innerHTML=
-`SVS begins in ${d}d ${h}h ${m}m`
-
-}
-
-setInterval(countdown,60000)
-countdown()
 
 function generateSlots(){
 
@@ -69,21 +29,20 @@ for(let h=0;h<24;h++){
 
 for(let m=0;m<60;m+=30){
 
-let time=
-String(h).padStart(2,"0")+":"+String(m).padStart(2,"0")
-let utcDate = new Date()
-utcDate.setUTCHours(h)
-utcDate.setUTCMinutes(m)
+let time=String(h).padStart(2,"0")+":"+String(m).padStart(2,"0")
 
-let local = utcDate.toLocaleTimeString([],{
-hour:'2-digit',
-minute:'2-digit'
-})
-
-  
 let id=currentBuff+"_"+time
 
 let div=document.createElement("div")
+
+let utcDate=new Date()
+utcDate.setUTCHours(h)
+utcDate.setUTCMinutes(m)
+
+let local=utcDate.toLocaleTimeString([],{
+hour:'2-digit',
+minute:'2-digit'
+})
 
 db.collection("slots").doc(id).onSnapshot(doc=>{
 
@@ -92,16 +51,16 @@ let data=doc.data()
 if(!data){
 
 div.className="slot available"
-
-div.innerHTML=`<b>${time} UTC</b><br>${local} Local<br>Available`
-
+div.innerHTML="<b>"+time+" UTC</b><br>"+local+" Local<br>Available"
 div.onclick=()=>openModal(id)
 
 }else{
 
+let color=getAllianceColor(data.alliance)
+
 div.className="slot reserved"
 
-div.innerHTML=`<b>${time} UTC</b><br>${local} Local<br>[${data.alliance}] ${data.player}`
+div.innerHTML="<b>"+time+" UTC</b><br>"+local+" Local<br><span style='color:"+color+"'>["+data.alliance+"]</span> "+data.player
 
 div.onclick=()=>cancelSlot(id,data.password)
 
@@ -137,11 +96,9 @@ let player=document.getElementById("player").value
 let password=document.getElementById("password").value
 
 db.collection("slots").doc(selectedSlot).set({
-
 alliance,
 player,
 password
-
 })
 
 closeModal()
@@ -153,89 +110,18 @@ function cancelSlot(id,password){
 let pass=prompt("Enter password")
 
 if(pass===ADMIN_PASSWORD){
-
 db.collection("slots").doc(id).delete()
 return
-
 }
 
 if(pass!==password){
-
 alert("Wrong password")
 return
-
 }
 
 db.collection("slots").doc(id).delete()
 
 }
-
-function switchBuff(buff){
-
-currentBuff=buff
-generateSlots()
-
-}
-
-const canvas=document.getElementById("snow")
-const ctx=canvas.getContext("2d")
-
-canvas.width=window.innerWidth
-canvas.height=window.innerHeight
-
-let snowflakes=[]
-
-for(let i=0;i<100;i++){
-snowflakes.push({
-x:Math.random()*canvas.width,
-y:Math.random()*canvas.height,
-r:Math.random()*3+1,
-d:Math.random()*1
-})
-}
-
-function drawSnow(){
-
-ctx.clearRect(0,0,canvas.width,canvas.height)
-
-ctx.fillStyle="rgba(180,200,255,0.8)"
-ctx.beginPath()
-
-for(let i=0;i<snowflakes.length;i++){
-
-let f=snowflakes[i]
-
-ctx.moveTo(f.x,f.y)
-ctx.arc(f.x,f.y,f.r,0,Math.PI*2,true)
-
-}
-
-ctx.fill()
-moveSnow()
-
-}
-
-function moveSnow(){
-
-for(let i=0;i<snowflakes.length;i++){
-
-let f=snowflakes[i]
-
-f.y+=f.d
-f.x+=Math.sin(f.y*0.01)
-
-if(f.y>canvas.height){
-f.y=0
-f.x=Math.random()*canvas.width
-}
-
-}
-
-}
-
-setInterval(drawSnow,33)
-
-generateSlots()
 
 function updateStats(){
 
@@ -258,9 +144,7 @@ counts[data.alliance]++
 let html=""
 
 for(let a in counts){
-
 html+=a+" : "+counts[a]+" slots<br>"
-
 }
 
 document.getElementById("stats").innerHTML=html
@@ -269,4 +153,5 @@ document.getElementById("stats").innerHTML=html
 
 }
 
+generateSlots()
 updateStats()
